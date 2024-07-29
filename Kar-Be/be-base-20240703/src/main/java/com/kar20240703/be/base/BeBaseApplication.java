efd8ab1fb1363355eaae99816258fd7ba8359c87
曹桂铭 <caoguiming.cq@chinatelecom.cn>
2024-07-29 15:36:02 +0800

    【i】【优化】【代码】

---
+++ Kar-Be/be-base-20240703/src/main/java/com/kar20240703/be/base/BeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.kar20240703.be.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeBaseApplication.class, args);
+    }
+
+}
