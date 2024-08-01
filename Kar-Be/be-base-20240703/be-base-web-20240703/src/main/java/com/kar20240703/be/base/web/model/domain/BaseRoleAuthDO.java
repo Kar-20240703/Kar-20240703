bbce31c6e7c90eb06d9e966a88b9370bf5478daf
曹桂铭 <caoguiming.cq@chinatelecom.cn>
2024-08-01 16:47:39 +0800

    【i】【优化】【代码】

--- Kar-Be/be-base-20240703/be-base-web-20240703/src/main/java/com/kar20240703/be/base/web/model/domain/BaseRoleAuthDO.java
+++ Kar-Be/be-base-20240703/be-base-web-20240703/src/main/java/com/kar20240703/be/base/web/model/domain/BaseRoleAuthDO.java
@@ -1,5 +1,7 @@
 package com.kar20240703.be.base.web.model.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -9,10 +11,11 @@ import lombok.Data;
 @Schema(description = "关联表：角色表，权限表")
 public class BaseRoleAuthDO {
 
-    @Schema(description = "权限主键id")
-    private Long authId;
-
+    @TableId(type = IdType.INPUT)
     @Schema(description = "角色主键id")
     private Long roleId;
+
+    @Schema(description = "权限主键id")
+    private Long authId;
 
 }
